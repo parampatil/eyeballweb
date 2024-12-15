2d9fa84c4df0cdeb5021d5f6d3c385576fcc7c63
ParamPatil <paramrpatil@gmail.com>
2024-12-14 19:31:55 -0500

    Added Link to github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
 import LetterPullup from "@/components/ui/letter-pullup";
 import { FadeText } from "@/components/ui/fade-text";
 import { RainbowButton } from "@/components/ui/rainbow-button";
+import { FaDownload, FaGithub, FaXmark } from "react-icons/fa6";
+import { TbFileTypeZip } from "react-icons/tb";
+import { useState } from "react";
 
 const Header = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
     <header className="flex justify-between items-center mb-2 px-4 border-b border-appaccent shadow-lg shadow-appaccent">
       <div className="flex items-end justify-start gap-2">
@@ -20,15 +25,34 @@ const Header = () => {
           className="text-red-600 text-xs"
         />
       </div>
-      <div className="p-2">
+      <div className="p-2 flex gap-2">
+          <div className="flex gap-2">
+            <RainbowButton
+              className={`hover:opacity-50 transition-all duration-300 ${showOptions ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}`}
+              onClick={() =>
+                (window.location.href =
+                  "https://github.com/parampatil/eyeball-software/releases/download/v0.1.0/eyeball_project.exe")
+              }
+            >
+              <TbFileTypeZip className="m-auto  mr-1" />
+              Download Software
+            </RainbowButton>
+            <RainbowButton
+              className={`hover:opacity-50 transition-all duration-300 ${showOptions ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}`}
+              onClick={() =>
+                (window.location.href =
+                  "https://github.com/parampatil/eyeball-software")
+              }
+            >
+              <FaGithub className="m-auto mr-1" />
+              View Source Code
+            </RainbowButton>
+          </div>
         <RainbowButton
+          onClick={() => setShowOptions(!showOptions)}
           className="hover:opacity-50 transition-opacity h-auto"
-          onClick={() =>
-            (window.location.href =
-              "https://github.com/parampatil/eyeball-software/releases/download/v0.1.0/eyeball_project.exe")
-          }
         >
-          Download Software
+          {showOptions ? <FaXmark className="m-auto" /> : <FaDownload className="m-auto" />}
         </RainbowButton>
       </div>
     </header>
